f761aec2c08e52747b2bf3df307b276b0dd99eec
Philip Dow <phil@phildow.net>
2018-08-22 12:19:35 -0700

    remove unused pixel normalization and denormalization structs parsing, just need the functions

--- Net Runner/Tensor IO/TIO Layer Interface/TIOPixelBufferLayerDescription.h
+++ Net Runner/Tensor IO/TIO Layer Interface/TIOPixelBufferLayerDescription.h
@@ -42,32 +42,18 @@ NS_ASSUME_NONNULL_BEGIN
 @property (readonly) TIOImageVolume shape;
 
 /**
- * A description of how pixel values will be normalized from a uint8_t range of `[0,255]` to some
- * other floating point range, may be `kTIOPixelNormalizationNone`.
- */
-
-@property (readonly) TIOPixelNormalization normalization;
-
-/**
  * A function that normalizes pixel values from a uint8_t range of `[0,255]` to some other
- * floating point range. may be `nil`.
+ * floating point range, may be `nil`.
  */
 
 @property (nullable, readonly) TIOPixelNormalizer normalizer;
-
-/**
- * A description of how pixel values will be denormalized from some floating point range back to
- * uint8_t values in the range `[0,255]`, may be `kTIOPixelDenormalizationNone`.
- */
-
-@property (readonly) TIOPixelDenormalization denormalization;
 
 /**
  * A function that denormalizes pixel values from a floating point range back to uint8_t values
  * in the range `[0,255]`, may be nil.
  */
 
-@property (nullable, readonly) TIOPixelDenormalizer denormalizer;
+@property (nullable, readonly) TIOPixelDenormalizer denormalizer ;
 
 /**
  * Designated initializer. Creates a pixel buffer description from the properties parsed in a
@@ -83,13 +69,7 @@ NS_ASSUME_NONNULL_BEGIN
  * @return instancetype A read-only instance of `TIOPixelBufferLayerDescription`
  */
 
-- (instancetype)initWithPixelFormat:(OSType)pixelFormat
-    shape:(TIOImageVolume)shape
-    normalization:(TIOPixelNormalization)normalization
-    normalizer:(nullable TIOPixelNormalizer)normalizer
-    denormalization:(TIOPixelDenormalization)denormalization
-    denormalizer:(nullable TIOPixelDenormalizer)denormalizer
-    quantized:(BOOL)quantized NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithPixelFormat:(OSType)pixelFormat shape:(TIOImageVolume)shape normalizer:(nullable TIOPixelNormalizer)normalizer denormalizer:(nullable TIOPixelDenormalizer)denormalizer quantized:(BOOL)quantized NS_DESIGNATED_INITIALIZER;
 
 /**
  * Use the designated initializer.
