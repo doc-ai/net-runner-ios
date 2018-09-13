15265306cda39a3dec60b205653e996c1306e92b
Philip Dow <phil@phildow.net>
2018-09-12 20:18:33 -0700

    initial ui for model import

---
+++ Net Runner/Settings/AddModelTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddModelTableViewController.h
+//  Net Runner
+//
+//  Created by Philip Dow on 9/12/18.
+//  Copyright © 2018 doc.ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddModelTableViewController : UITableViewController <UITextFieldDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
